7a9ededc8e91f7b07bd3579ff431ab49904606a9
jinjidecj <1972922090@qq.com>
2019-07-23 21:54:10 +0800

    .

---
+++ src/main/java/com/mti/webstore/mtistore/MtistoreApplication.java
@@ -0,0 +1,13 @@
+package com.mti.webstore.mtistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtistoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtistoreApplication.class, args);
+    }
+
+}
